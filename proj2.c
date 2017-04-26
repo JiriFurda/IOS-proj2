09439c444a9ae0fe75e6ffe1ab580caba6d657e7
Jiří Furda <Furdys@users.noreply.github.com>
2017-04-26 19:17:22 +0200

    Test fork

--- proj2.c
+++ proj2.c
@@ -4,21 +4,26 @@ Autor: Jiri Furda (xfurda00)
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
-	int aParam;
-	int cParam;
-	int agtParam;
-	int cgtParam;
-	int watParam;
-	int cwtParam;
-	
 	if(argc != 7)
 	{
 		fprintf(stderr,"Error: Invalid argument count\n");
 		return 1;
 	}
+	
+	int aParam = atoi(argv[1]); // test when not a number
+	int cParam = atoi(argv[2]);
+	int agtParam = atoi(argv[3]);
+	int cgtParam = atoi(argv[4]);
+	int awtParam = atoi(argv[5]);
+	int cwtParam = atoi(argv[6]);
+	
+	
 	
 	if(!(aParam > 0))
 	{
@@ -49,6 +54,27 @@ int main(int argc, char *argv[])
 	{
 		fprintf(stderr,"Error: Parameter \"CWT\" must be number between 0 and 5001\n");
 		return 1;
+	}
+	
+	
+	int pid = fork();
+	
+	if(pid == 0)
+	{
+		printf("I'm child (%d)\n",pid);
+		// exec(....), exit(exitcode)
+	}
+	else if(pid == -1)
+	{
+		fprintf(stderr,"Error: Fork failed\n");
+		errnum = errno;
+		fprintf(stderr, "Error #%d opening file: %s\n", errnum, strerror(errnum));
+		return 1;
+	}
+	else
+	{
+		printf("I'm parent (child=%d)\n",pid);
+		// pid2 = wait(&stav);
 	}
 
 	return 0;
